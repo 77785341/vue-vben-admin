f14ef1a18526b8e60eb4e25a4f1844bc5e672b9d
Felix <77785341@qq.com>
2026-03-23 16:17:43 +0800

    fix: 111

--- apps/web-antd/src/router/routes/modules/demos.ts
+++ apps/web-antd/src/router/routes/modules/demos.ts
@@ -1,3 +1,13 @@
+/*
+ * @Author: Felix 77785341@qq.com
+ * @Date: 2026-03-23 15:15:46
+ * @LastEditors: Felix 77785341@qq.com
+ * @LastEditTime: 2026-03-23 16:17:15
+ * @FilePath: \vue-vben-admin\apps\web-antd\src\router\routes\modules\demos.ts
+ * @Description: 
+ * 
+ * Copyright (c) 2026 by ${git_name_email}, All Rights Reserved. 
+ */
 import type { RouteRecordRaw } from 'vue-router';
 
 import { $t } from '#/locales';
@@ -10,7 +20,7 @@ const routes: RouteRecordRaw[] = [
       order: 1000,
       title: $t('demos.title'),
     },
-    name: 'Demos',
+    name: 'Demos1',
     path: '/demos',
     children: [
       {
